2fb6554c4991c924fd13e7f127be09ea686aff90
KindredSoul <jabriltb2@gmail.com>
2020-11-05 16:24:35 -0600

    Implemented removeItem function

--- src/App.js
+++ src/App.js
@@ -22,10 +22,14 @@ function App() {
 		])
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(cartItem => cartItem.id !== id ))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<CartContext.Provider value={{cart}}>
+				<CartContext.Provider value={{cart, removeItem}}>
 					<Navigation />
 
 					{/* Routes */}
